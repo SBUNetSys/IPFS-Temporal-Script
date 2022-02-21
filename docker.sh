66768ef9bfe7e582a8451bd3ddcfdc5b3e6babb7
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2022-02-21 14:11:04 -0500

    docker now mount data into images

--- docker.sh
+++ docker.sh
@@ -4,6 +4,7 @@ sudo sysctl -w net.core.rmem_max=2500000
 docker run -d --name $(date +%F)-dtube\
        -v $(pwd)/result:/result \
        -v $(pwd)/scripts:/scripts \
+       -v $(pwd)/data:/data \
        --cap-add=NET_ADMIN \
        dtube-script
 
