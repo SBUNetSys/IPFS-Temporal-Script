9de2aec83a7a73e04a39c13b2d5497171562c7cd
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2022-02-23 10:36:00 -0500

    - updated sample cids to 200

--- data/random_cids.py
+++ data/random_cids.py
@@ -6,8 +6,8 @@ def main():
     with open('all_files.json', 'r') as fin:
         all_files = json.load(fin)
     all_cids = all_files.keys()
-    # randomly pick 500 cids
-    random_cids = random.sample(all_cids, 500)
+    # randomly pick 200 cids
+    random_cids = random.sample(all_cids, 200)
     with open('random_sample_files_cid.txt', 'w') as fout:
         fout.write('\n'.join(random_cids))
 
