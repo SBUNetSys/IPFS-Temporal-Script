d0e9d28e4170a2d6321f4ee97b9f9b167c9d4b31
ZhengYu Wu <33101936+w774908117@users.noreply.github.com>
2022-03-07 14:38:10 -0500

    Updated docker container naming

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
 mkdir -p result
 docker build -t ipfs-temporal-script .
 sudo sysctl -w net.core.rmem_max=2500000
-docker run -d --name $(date +%F)-ipfs-temporal\
+docker run -d --name $(date +%F-%H)-ipfs-temporal\
        -v $(pwd)/result:/result \
        -v $(pwd)/scripts:/scripts \
        -v $(pwd)/data:/data \
