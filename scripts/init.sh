60b53e2d3c346d27a4161dc6cbc17dfdf408bbfc
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2022-03-15 19:21:05 -0400

    - updated init script now works with remote task

---
+++ scripts/init.sh
@@ -0,0 +1,14 @@
+cd /result
+#mkdir -p $(date +%F)
+mkdir -p $DIR_NAME
+# start record hop information
+cd $DIR_NAME
+echo $(pwd)
+~/ipfs_bin/ipfs repo gc
+#~/ipfs_bin/ipfs daemon --enable-gc > $(date +%F)_daemon.txt 2>&1 &
+~/ipfs_bin/ipfs daemon --enable-gc > ${DIR_NAME}_daemon.txt 2>&1 &
+sleep 30
+~/ipfs_bin/ipfs log level dht warn
+~/ipfs_bin/ipfs log level bitswap warn
+python /scripts/record.py -d "${DIR_NAME}" -f "${FILE_NAME}" -s "${HOST_IP}" -p "${HOST_PORT}" -t "${TASK_NAME}"
+killall ipfs
