6739ca0ca4444874541b83e8adfd6d0c077773b2
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2022-03-15 19:34:27 -0400

    - added sending task to server

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -1,9 +1,13 @@
+import argparse
+import json
 import os
 import shutil
 import datetime
 
+import requests
 
-def main(dir_prefix):
+
+def main(dir_prefix, host, port):
     now = datetime.datetime.now()
     # check if collecting period ends or not
     with open(os.path.join(dir_prefix, 'collect_period.txt'), 'r') as fin:
@@ -19,6 +23,9 @@ def main(dir_prefix):
     with open(file_path, 'r') as fin:
         for line in fin.readlines():
             new_cids.append(line.replace("\n", ""))
+    # CAP to 1000
+    if len(new_cids) > 1000:
+        new_cids = new_cids[:1000]
     # load global dataset
     global_db_path = os.path.join(dir_prefix, 'all_data.txt')
     global_db = []
@@ -36,7 +43,23 @@ def main(dir_prefix):
     daily_file_path = os.path.join(dir_prefix, f'{current_day}_cids.txt')
     shutil.copy(global_db_path, daily_file_path)
 
+    # send to server
+    # case of free machine
+    url = f'http://{host}:{port}/addTask'
+    data = {
+        'folder_name': dir_prefix,
+        'file_name': f'{current_day}_cids.txt'}
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    res = requests.post(url, data=json.dumps(data), headers=headers)
+    if res.status_code != 200:
+        print("Error on sending request")
+
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--server', type=str, help='central server ip', required=True)
+    parser.add_argument('-p', '--port', type=str, help='central host port', required=True)
+    args = parser.parse_args()
+
     prefix = './collected_data'
-    main(prefix)
+    main(prefix, args.server, args.port)
