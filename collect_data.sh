2ba6d922bf10e954282e24a824643e1c719a405b
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2022-03-19 15:48:06 -0400

    - bug fix on ipfs collect scripting machine

--- collect_data.sh
+++ collect_data.sh
@@ -2,5 +2,5 @@
 source /home/ax/opt/anaconda3/etc/profile.d/conda.sh
 conda activate py3
 cd /home/ax/workspace/IPFS-Temporal-Script/
-python ./data/record_daily.py
+python ./scripts/collect_data.py
 
--- scripts/init.sh
+++ scripts/init.sh
@@ -4,10 +4,11 @@ mkdir -p $DIR_NAME
 # start record hop information
 cd $DIR_NAME
 echo $(pwd)
+~/ipfs_bin/ipfs config show
 ~/ipfs_bin/ipfs repo gc
 #~/ipfs_bin/ipfs daemon --enable-gc > $(date +%F)_daemon.txt 2>&1 &
 ~/ipfs_bin/ipfs daemon --enable-gc > ${DIR_NAME}_daemon.txt 2>&1 &
-sleep 30
+sleep 600
 ~/ipfs_bin/ipfs log level dht warn
 ~/ipfs_bin/ipfs log level bitswap warn
 python /scripts/record.py -d "${DIR_NAME}" -f "${FILE_NAME}" -s "${HOST_IP}" -p "${HOST_PORT}" -t "${TASK_NAME}"
